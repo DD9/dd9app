abf73a5fdf999ad3215aaada32dd861d8924167b
Conner Simmering <connersimmering@gmail.com>
2018-09-23 22:40:06 -0600

    tweak google strategy settings to allow proxies

--- helpers/passport.js
+++ helpers/passport.js
@@ -15,6 +15,7 @@ passport.use(new GoogleStrategy({
   clientID: process.env.GOOGLE_CLIENT_ID,
   clientSecret: process.env.GOOGLE_CLIENT_SECRET,
   callbackURL: '/auth/google/callback',
+  proxy: true,
 },
 // Do after successful Google authentication
 (accessToken, refreshToken, profile, done) => {
